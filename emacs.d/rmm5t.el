cda7a4f1ded3363f46a710cd111fa484cc543a18
Paul Foster <paulfstr@gmail.com>
2013-09-06 14:23:43 +1000

    Add smooth scrolling

--- emacs.d/rmm5t.el
+++ emacs.d/rmm5t.el
@@ -27,6 +27,7 @@
 (load "rmm5t/mac")
 (load "rmm5t/server-mode")
 (load "rmm5t/shell-mode")
+(load "rmm5t/smoothscroll")
 (load "rmm5t/private" 'noerror)
 
 ;; (load "rmm5t/hl-line")
---
+++ emacs.d/rmm5t/smoothscroll.el
@@ -0,0 +1,4 @@
+; Smooth scrolling
+(setq scroll-step 1
+      scroll-conservatively 10000)
+(setq scroll-margin 7)
