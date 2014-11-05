0c4265197e4b7294e4314b4b0374d5f13632cdfd
Paul Foster <paulfstr@gmail.com>
2014-11-05 23:32:59 +1100

    Choose better flymake highlighting colors so that it doesn't make text unreadable

--- emacs.d/rmm5t/flymake.el
+++ emacs.d/rmm5t/flymake.el
@@ -1,4 +1,26 @@
 ;;; Flymake
 
-(defface flymake-errline '((t (:background "#330000" :overline "red" :underline "red"))) nil)
-(defface flymake-warnline '((t (:background "#333300" :overline "yellow" :underline "yellow"))) nil)
+;; (defface flymake-errline '((t (:background "#0000CC" :overline "red" :underline "red"))) nil)
+;; (defface flymake-warnline '((t (:background "#FF99FF" :overline "yellow" :underline "yellow"))) nil)
+;; (custom-set-faces
+;;  '(flymake-errline ((((class color)) (:underline "red"))))
+;;  '(flymake-warnline ((((class color)) (:underline "yellow")))))
+
+
+;; (defface flymake-errline '((t (:background "DarkRed" :overline "red" :underline "red"))) nil)
+;; ;; (defface flymake-warnline '((t (:background "#888888" :overline "red" :underline "red"))) nil)
+;; '(flymake-warnline ((((class color)) (:background "LightBlue2" :foreground "black"))))
+(custom-set-faces
+  '(flymake-errline ((((class color)) (:background "color-126"))))
+  '(flymake-warnline ((((class color)) (:background "Gray30"))))
+  '(flymake-infoline ((((class color)) (:background "color-97")))))
+
+
+;; GOOD
+;; (defface flymake-errline '((t (:background "#800000" :overline "red" :underline "red"))) nil)
+;; (defface flymake-warnline '((t (:background "#003300" :overline "yellow" :underline "yellow"))) nil)
+
+
+;; ORIGINALS:
+;; (defface flymake-errline '((t (:background "#330000" :overline "red" :underline "red"))) nil)
+;; (defface flymake-warnline '((t (:background "#333300" :overline "yellow" :underline "yellow"))) nil)
