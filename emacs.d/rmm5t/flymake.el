04f480129b6b5b94d7fd001618a7ffc375e8a78d
Paul Foster <paulfstr@gmail.com>
2014-11-07 23:11:52 +1100

    Tidy code by removing commented-out code when setting flymake colors

--- emacs.d/rmm5t/flymake.el
+++ emacs.d/rmm5t/flymake.el
@@ -6,21 +6,7 @@
 ;;  '(flymake-errline ((((class color)) (:underline "red"))))
 ;;  '(flymake-warnline ((((class color)) (:underline "yellow")))))
 
-
-;; (defface flymake-errline '((t (:background "DarkRed" :overline "red" :underline "red"))) nil)
-;; ;; (defface flymake-warnline '((t (:background "#888888" :overline "red" :underline "red"))) nil)
-;; '(flymake-warnline ((((class color)) (:background "LightBlue2" :foreground "black"))))
 (custom-set-faces
   '(flymake-errline ((((class color)) (:background "color-126"))))
   '(flymake-warnline ((((class color)) (:background "Gray30"))))
   '(flymake-infoline ((((class color)) (:background "color-97")))))
-
-
-;; GOOD
-;; (defface flymake-errline '((t (:background "#800000" :overline "red" :underline "red"))) nil)
-;; (defface flymake-warnline '((t (:background "#003300" :overline "yellow" :underline "yellow"))) nil)
-
-
-;; ORIGINALS:
-;; (defface flymake-errline '((t (:background "#330000" :overline "red" :underline "red"))) nil)
-;; (defface flymake-warnline '((t (:background "#333300" :overline "yellow" :underline "yellow"))) nil)
